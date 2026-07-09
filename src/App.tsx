844c1219b5e15371ed7042b6ee190592feddbcd0
hasanaj88 <alza0083@algonquinlive.com>
2026-07-09 12:22:20 -0400

    Fix React Router basename for Vite

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
               <Routes>
                 <Route path="/" element={<Index />} />
                 <Route path="*" element={<NotFound />} />
@@ -30,5 +30,5 @@ const App = () => (
     </div>
   </div>
 );
-export default App;
 
+export default App;
