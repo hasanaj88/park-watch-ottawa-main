f8cabaa8085d543a0d1a4a313c9a2561a9742877
hasanaj88 <alza0083@algonquinlive.com>
2026-07-14 14:19:43 -0400

    Fix Vite base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: "/park-watch-ottawa-main/",
+  base: "/",
+
   server: {
     host: "::",
     port: 8080,
     proxy: {
       "/api": "http://localhost:3001",
     },
-},
+  },
+
   plugins: [react()],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
-
